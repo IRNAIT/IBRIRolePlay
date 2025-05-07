807f582901cf31791769d6ecf8dee412c6ab213a
IRNAIT <mitrandil15@gmail.com>
2025-05-07 19:19:24 +0300

    Move server code to src directory

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Раздача статических файлов
+app.use(express.static(path.join(__dirname, '..')));
+
+// Маршрут для главной страницы
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+// Запуск сервера
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Сервер запущен на порту ${port}`);
+}); 
